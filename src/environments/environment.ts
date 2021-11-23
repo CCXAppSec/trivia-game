332f3e56b0ae64faf465af5a288ee11af268392f
Fadzayi <fadzayi@creacha.co.za>
2021-11-23 11:19:43 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,15 @@
 export const environment = {
   production: false,
   config: {
-    apiKey: "AIzaSyD90b0H3PwHKS1bv3VP54mjd55rWxYchnE", 
+    apiKey: "AIzaSyD90b0H3PwHKS1bv3VP54mjd55rWxYchnE",
     authDomain: "zimtrivia.firebaseapp.com",
     databaseURL: "https://zimtrivia-default-rtdb.firebaseio.com",
     projectId: "zimtrivia",
     storageBucket: "zimtrivia.appspot.com",
     messagingSenderId: "186683260969",
     appId: "1:186683260969:web:ceb841f9fcc41dab179c38"
+
+    // token goes here
   }
 };
 
